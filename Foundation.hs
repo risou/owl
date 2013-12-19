de4041adb378bc8d30aef8df6bc5a39ab326a2c9
risou <risou.f@gmail.com>
2013-12-20 01:56:10 +0900

    change font-family

--- Foundation.hs
+++ Foundation.hs
@@ -88,7 +88,7 @@ instance Yesod Owl where
 
         pc <- widgetToPageContent $ do
             $(widgetFile "normalize")
-            addStylesheet $ StaticR css_bootstrap_css
+            {-addStylesheet $ StaticR css_bootstrap_css-}
             $(widgetFile "owl-base")
             $(widgetFile "default-layout")
         hamletToRepHtml $(hamletFile "templates/default-layout-wrapper.hamlet")
